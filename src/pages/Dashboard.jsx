40c80b3f2246546a0e64e1fce9f27be9d89919d4
Nirajan Malla <nirajannitro5@gmail.com>
2023-09-23 16:38:27 +0545

    folder structure for dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,23 @@
-import { extractProfile } from '@stacks/profile';
 import { useSelector } from 'react-redux';
+import UserProfile from '../components/Dashboard/UserProfile';
 
 function Dashboard() {
   const { response } = useSelector((state) => state.wallet);
-  const tokenFR = response?.authResponse;
-  if (!tokenFR)
-    return;
-  const profile = extractProfile(tokenFR);
-  console.log(profile);
-  // const profileObject = new Profile(profile);
-  // console.log(profileObject);
-  return <div className='min-h-[70vh]'>hi</div>;
+  console.log(response);
+  return (
+    <div className='min-h-[70vh] max-w-screen-xl mx-auto flex flex-col'>
+      <UserProfile />
+    </div>
+  );
 }
 
 export default Dashboard;
+
+
+// const tokenFR = response?.authResponse;
+// if (!tokenFR)
+//   return;
+// const profile = extractProfile(tokenFR);
+// console.log(profile);
+// const profileObject = new Profile(profile);
+// console.log(profileObject);
