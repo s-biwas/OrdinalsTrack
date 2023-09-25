90843427bee399109f6a99ab7b0b0932182a0da8
Dawa Sherpa <56500281+dawasherpa-ui@users.noreply.github.com>
2023-09-25 21:02:00 +0545

    detail layout

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -27,57 +27,65 @@ export default function Detail() {
     };
 
     return (
-        <div className="max-w-screen-xl min-h-[70vh] mx-auto flex flex-col md:flex-row my-14 gap-5">
-            <div className="basis-3/12 h-fit rounded-md">
+        <div className="max-w-screen-xl min-h-[70vh] mx-auto flex flex-col lg:flex-row my-14 gap-5">
+            <div className="w-full lg:w-1/2 h-fit rounded-md">
                 {Details && (
                     <ContentDisplay
                         id={Details.id}
                         content_type={Details.content_type}
+                        className="object-cover w-full h-full"
                     />
                 )}
             </div>
-            <div className="basis-3/12 h-fit rounded-md bg-[#222] p-2">
-                {Transfers && Transfers.results.length > 0 ? (
-                    <div>
-                        <h2 className="text-xl font-semibold mb-4">Transfer Details</h2>
-                        {Transfers.results.map((transfer) => (
-                            <div key={transfer.tx_id} className="mb-6">
-                                <p><strong className="text-blue-500">Transfer ID:</strong> {transfer.tx_id}</p>
-                                <p><strong   className="text-blue-500">Block Height:</strong> {transfer.block_height}</p>
-                                <p><strong  className="text-blue-500">Block Hash:</strong> {transfer.block_hash}</p>
-                                <p><strong  className="text-blue-500">Address:</strong> {transfer.address}</p>
-                                <p><strong  className="text-blue-500">Value:</strong> {transfer.value}</p>
-                                <p><strong  className="text-blue-500">Timestamp:</strong> {new Date(transfer.timestamp).toLocaleString()}</p>
-                                <hr className="my-2" />
-                            </div>
-                        ))}
-                    </div>
-                ) : (
-                    <p>No transfers available</p>
-                )}
-            </div>
-            <div className="basis-3/12 h-fit rounded-md bg-[#333] p-2 text-gray-200">
-                {Details && (
-                    <div>
-                        <h2 className="text-xl font-semibold mb-4">OrdinasInscription Details</h2>
-                        <p><strong className="text-green-500">OrdinasInscription ID:</strong> {Details.id}</p>
-                        <p><strong className="text-green-500">Number:</strong> {Details.number}</p>
-                        <p><strong className="text-green-500">Address:</strong> {Details.address}</p>
-                        <p><strong className="text-green-500">Sat Ordinal:</strong> {Details.sat_ordinal}</p>
-                        <p><strong className="text-green-500">Sat Rarity:</strong> {Details.sat_rarity}</p>
-                        <p><strong className="text-green-500">Sat Coinbase Height:</strong> {Details.sat_coinbase_height}</p>
-                        <p><strong className="text-green-500">Timestamp:</strong> {new Date(Details.timestamp).toLocaleString()}</p>
-                      
-                        <button
-                            className=" text-white px-2 py-1 rounded  cursor-pointer mt-4"
-                            onClick={() => copyToClipboard(Details.address)}
-                            title="Copy Address to Clipboard"
-                        >
-                            <img src={CopyIcon} alt="Copy" className="w-4 h-4 mr-1" />
-                         
-                        </button>
-                    </div>
-                )}
+            <div className="w-full lg:w-1/2 h-fit rounded-md bg-black-400 text-white p-2 shadow-lg">
+                <div className="flex flex-col gap-4">
+                    {Details && (
+                        <div>
+                            <h2 className="text-xl font-semibold mb-4">Ordinal's Inscription Details</h2>
+                            <p><strong className="text-green-500">Ordinal's Inscription ID:</strong> <span style={{wordBreak: 'break-all'}}>{Details.id}</span></p>
+                            <p><strong className="text-green-500">Number:</strong> <span style={{wordBreak: 'break-all'}}>{Details.number}</span></p>
+                            <p className="flex items-baseline"><strong className="text-green-500">Address:</strong> <span style={{wordBreak: 'break-all'}}>{Details.address}</span>
+                                <button
+                                    className=" text-white px-2 py-1 rounded  cursor-pointer ml-2"
+                                    onClick={() => copyToClipboard(Details.address)}
+                                    title="Copy Address to Clipboard"
+                                >
+                                    <img src={CopyIcon} alt="Copy" className="w-4 h-4" />
+                                </button>
+                            </p>
+                            <p><strong className="text-green-500">Sat Ordinal:</strong> <span style={{wordBreak: 'break-all'}}>{Details.sat_ordinal}</span></p>
+                            <p><strong className="text-green-500">Sat Rarity:</strong> <span style={{wordBreak: 'break-all'}}>{Details.sat_rarity}</span></p>
+                            <p><strong className="text-green-500">Sat Coinbase Height:</strong> <span style={{wordBreak: 'break-all'}}>{Details.sat_coinbase_height}</span></p>
+                            <p><strong className="text-green-500">Timestamp:</strong> <span style={{wordBreak: 'break-all'}}>{new Date(Details.timestamp).toLocaleString()}</span></p>
+                        </div>
+                    )}
+                    {Transfers && Transfers.results.length > 0 ? (
+                        <div>
+                            <h2 className="text-xl font-semibold mb-4">Transfer Details</h2>
+                            {Transfers.results.map((transfer) => (
+                                <div key={transfer.tx_id} className="mb-6">
+                                    <p><strong className="text-blue-500">Transfer ID:</strong> <span style={{wordBreak: 'break-all'}}>{transfer.tx_id}</span></p>
+                                    <p><strong className="text-blue-500">Block Height:</strong> <span style={{wordBreak: 'break-all'}}>{transfer.block_height}</span></p>
+                                    <p><strong className="text-blue-500">Block Hash:</strong> <span style={{wordBreak: 'break-all'}}>{transfer.block_hash}</span></p>
+                                    <p className="flex items-baseline"><strong className="text-blue-500">Address:</strong> <span style={{wordBreak: 'break-all'}}>{transfer.address}</span>
+                                        <button
+                                            className=" text-white px-2 py-1 rounded  cursor-pointer ml-2"
+                                            onClick={() => copyToClipboard(transfer.address)}
+                                            title="Copy Address to Clipboard"
+                                        >
+                                            <img src={CopyIcon} alt="Copy" className="w-4 h-4" />
+                                        </button>
+                                    </p>
+                                    <p><strong className="text-blue-500">Value:</strong> <span style={{wordBreak: 'break-all'}}>{transfer.value}</span></p>
+                                    <p><strong className="text-blue-500">Timestamp:</strong> <span style={{wordBreak: 'break-all'}}>{new Date(transfer.timestamp).toLocaleString()}</span></p>
+                                    <hr className="my-2" />
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <p>No transfers available</p>
+                    )}
+                </div>
             </div>
         </div>
     );
