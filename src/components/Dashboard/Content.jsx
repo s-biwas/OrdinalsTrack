a931be2a8f2a48f7121e434869334d9205dbfbcc
Nirajan Malla <nirajannitro5@gmail.com>
2023-09-25 14:01:08 +0545

    desiging content

--- src/components/Dashboard/Content.jsx
+++ src/components/Dashboard/Content.jsx
@@ -56,36 +56,38 @@ function ContentDisplay({ id, content_type, number }) {
             setContentElement(<img
                 src={contentUrl}
                 alt="Ordinal Image"
-                className="w-full h-full"
+                className="w-full h-full rounded-md"
             />);
         }
         else if (content_type.startsWith('video/')) {
             setContentElement(<video
                 controls
                 src={contentUrl}
+                className="rounded-md"
             />);
         }
         else if (content_type.startsWith('audio/')) {
             setContentElement(<audio
                 controls
                 src={contentUrl}
+                className="rounded-md"
             />);
         }
         else if (content_type.startsWith('text/')) {
             fetchTextContent(contentUrl)
                 .then((textContent) => {
-                    setContentElement(<pre className="bg-slate-200/70 h-full w-full overflow-scroll p-5 text-black custom-scrollbar">{textContent}</pre>);
+                    setContentElement(<pre className="bg-slate-200/80 h-full text-Grey9 w-full overflow-scroll p-5 custom-scrollbar rounded-md">{textContent}</pre>);
                 })
                 .catch((error) => {
                     console.error('Error fetching text content:', error);
                     setContentElement(
-                        <pre className=" h-full w-fulloverflow-scroll custom-scrollbar p-5 text-white border bg-slate-200/10">Something went wrong ʕ•̠͡•ʔ</pre>);
+                        <pre className=" h-full w-full overflow-scroll p-5 text-white border bg-slate-200/10">Something went wrong ʕ•̠͡•ʔ</pre>);
                 });
         }
         else {
             fetchJsonContent(contentUrl)
                 .then((textContent) => {
-                    setContentElement(<pre className="bg-slate-200/70 h-full w-full overflow-scroll p-5 text-black custom-scrollbar">{textContent}</pre>);
+                    setContentElement(<pre className="bg-slate-200/80 h-full text-Grey9 w-full overflow-scroll p-5 custom-scrollbar rounded-md">{textContent}</pre>);
                     console.log(contentElement);
                 })
                 .catch((error) => {
@@ -97,9 +99,9 @@ function ContentDisplay({ id, content_type, number }) {
     }, []);
 
     return (
-        <div className="group-hover:scale-105 text-white w-full h-full relative min-h-[130px]">
+        <div className="group-hover:scale-105 text-white w-full h-full relative min-h-[130px] ">
             {contentElement}
-            <span className="absolute bottom-2 left-2 px-2 py-1 bg-slate-100/70 text-slate-800">#{number}</span>
+            <span className="absolute bottom-2 left-2 px-2 py-1 bg-slate-100/30 rounded-md text-slate-800">#{number}</span>
         </div>
     );
 }
